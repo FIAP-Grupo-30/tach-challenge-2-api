fa75c3d65a2b5d6642bf35e1a5f9fb3b5031c23b
Vitor Oliveira <vitorsaop@gmail.com>
2025-12-15 21:20:21 -0300

    feat - primeiro commit

---
+++ src/test/java/tach_challenge_2/api_financeiro/ApiFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package tach_challenge_2.api_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
